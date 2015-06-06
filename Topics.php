4c0f66fb3de5243eece1087c94bd7a31362f1b71
John Lockman III <jlockman3@gmail.com>
2015-06-06 18:49:18 -0500

    added so much stuff i can't comment

--- Topics.php
+++ Topics.php
@@ -29,6 +29,41 @@ class Topics {
         <?php
     } // end topicsInit ()
 
+		public function homeInit () {
+
+			?>
+					<h1>this is the homepage for Council Connect</h1>
+
+    		<?php
+		} // end homepage ()
+
+		function aboutInit () {
+
+		?>
+        <div>
+            <h1>About Council Connect</h1>
+
+				<p>This project will help the citizens of Austin participate in City Council decision making by providing notifications of upcoming agenda items and deadlines to sign up for public testimony. This will enable citizens to keep tabs on decision making so they can participate before decisions are finalized because Council information is currently too fragmented across different pages on the City's website.</p>
+
+			<p>We expect to have notifications for future items, the ability to search past items with suggested tags and a news feature combined in one app to make it easier for citizens to follow City Council events. Demo of basic functionality <a href='https://www.youtube.com/watch?v=s1Mg3bwntkU' TARGET='blank'>[here]</a>.</p>
+
+				<h3>HELP WANTED</h3>
+				<ul>
+					<li>UI </li>
+					<li>WordPress</li>
+					<li>Experts who know the city's agenda management software or are familiar with the industry more generally </li>
+					<li>Citizens who have followed a decision through the Council's process</li>
+					<li>Wonky types interested in sorting through the city's website to trace out workflow as much as possible</li>
+					<li>Writers to document our work today as well as the reqs for features discussed but not viable with current data structure.</li>
+				</ul>
+<hr />
+				<h3>Description of the Code Design</h3>
+			<p>Austin City Council's website is scraped to extract meeting agenda items.  The items are then loaded into a wordpress/bbpress framework.  The backend is written using PHP to control the flow and interface with the MySql database.  Client side code is written in JQuery to standarize browser behavior for DOM manimpulation and event handling.  Responsive Bootstrap classes are used to make the front end compatible with a variety of device types and sizes.  </p>
+        </div>
+
+        <?php
+    } // end topicsInit ()
+
     //---------------------
     function topicsShow () {
 
