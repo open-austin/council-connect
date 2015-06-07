38afa61bbff386f9d80a49db39e6dd7533b58812
John Lockman III <jlockman3@gmail.com>
2015-06-06 20:23:35 -0500

    adding Sammy's images

--- footer.php
+++ footer.php
@@ -1,6 +1,26 @@
 <!-- footer.php  -->
-        <div id="footer">
-            <!--h1>FOOTER</h1-->
+        
+    </div>
+    <div id="footer">
+        <div class="left">
+        	<div>
+	        	<p>Made with love by participants of the ATX Hack4Change <br>
+	        	Questions? E-mail us at ||Put email here||</p>
+	        </div>
+        </div>
+        <div class="right">
+        	<div>
+	        	<p>Find us on:</p>
+	        	<span class="icon">
+	        		<img src="<?php echo get_template_directory_uri();?>/img/face.png" width="36"  height="36"/>
+	        	</span>
+	        	<span class="icon">
+	        		<img src="<?php echo get_template_directory_uri();?>/img/twitter.png" width="36"  height="36"/>
+	        	</span>
+	        	<span class="icon">
+	        		<img src="<?php echo get_template_directory_uri();?>/img/insta.png" width="36"  height="36"/>
+	        	</span>
+	        <div>
         </div>
     </div>
 </body>
