38a93cef7f49fe207fa3e1fe23e4de4cd53b6f0e
T. Greg O'Neil <tgregoneil@gmail.com>
2015-06-10 15:01:01 -0500

    footer.php updated

--- footer.php
+++ footer.php
@@ -5,21 +5,21 @@
         <div class="left">
         	<div>
 	        	<p>Made with love by participants of the ATX Hack4Change <br>
-	        	Questions? E-mail us at ||Put email here||</p>
 	        </div>
         </div>
         <div class="right">
         	<div>
-	        	<p>Find us on:</p>
+	        	<p>Connect:
 	        	<span class="icon">
-	        		<img src="<?php echo get_template_directory_uri();?>/img/face.png" width="36"  height="36"/>
+	        		<a href="http://www.facebook.com/ATXCouncilConn" border=0 target="blank"><img src="<?php echo get_template_directory_uri();?>/img/face.png" width="36"  height="36"/></a>
 	        	</span>
 	        	<span class="icon">
-	        		<img src="<?php echo get_template_directory_uri();?>/img/twitter.png" width="36"  height="36"/>
+	        		<a href="http://twitter.com/ATXCouncilConn" border=0 target="blank"><img src="<?php echo get_template_directory_uri();?>/img/twitter.png" width="36"  height="36"/></a>
 	        	</span>
 	        	<span class="icon">
-	        		<img src="<?php echo get_template_directory_uri();?>/img/insta.png" width="36"  height="36"/>
+	        		<a href="https://instagram.com/atxcouncilconn" border=0 target="blank"><img src="<?php echo get_template_directory_uri();?>/img/insta.png" width="36"  height="36"/></a>
 	        	</span>
+</p>
 	        <div>
         </div>
     </div>
